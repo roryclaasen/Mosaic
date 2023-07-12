755f8f0d9a8ed8c716a9c0f1205148bce349cfd9
Rory Claasen <rory@xbox.com>
2023-07-12 16:29:35 +0100

    Formats solution and adds missing settings

--- Mosaic.Infrastructure/MosaicManager.cs
+++ Mosaic.Infrastructure/MosaicManager.cs
@@ -9,7 +9,6 @@ namespace Mosaic.Infrastructure
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
     using LibVLCSharp.Shared;
     using Mosaic.Infrastructure.Config;
     using Mosaic.Infrastructure.Events;
