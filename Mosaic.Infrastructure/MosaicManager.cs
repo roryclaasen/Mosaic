8b51bf318f771cd105390c7c8f785e8ff393dd82
Rory Claasen <rory@claasen.biz>
2023-07-18 19:38:45 +0100

    quick small refactor

--- Mosaic.Infrastructure/MosaicManager.cs
+++ Mosaic.Infrastructure/MosaicManager.cs
@@ -7,23 +7,22 @@
 namespace Mosaic.Infrastructure
 {
     using System;
-    using System.Collections.Generic;
     using Mosaic.Infrastructure.Collections;
     using Mosaic.Infrastructure.Config;
 
     public partial class MosaicManager
     {
-        private readonly ConcurrentLoopingQueue<MediaEntry> weightedBag = new();
+        private readonly ConcurrentLoopingQueue<MediaEntry> loopingQueue = new();
 
         private MosaicConfig? config;
 
-        public int SourceCount => this.weightedBag.Count;
+        public int SourceCount => this.loopingQueue.Count;
 
         public void SetConfig(MosaicConfig config)
         {
             this.config = config;
-            this.weightedBag.Clear();
-            this.weightedBag.EnqueueRange(config.Sources);
+            this.loopingQueue.Clear();
+            this.loopingQueue.EnqueueRange(config.Sources);
         }
 
         public void StartTile(IVideoPlayerTile tile)
@@ -38,7 +37,7 @@ namespace Mosaic.Infrastructure
 
         public void TriggerNextVideo(IVideoPlayerTile tile)
         {
-            if (this.weightedBag?.TryDequeue(out var source) ?? false)
+            if (this.loopingQueue.TryDequeue(out var source))
             {
                 tile.PlayVideo(new Uri(source.Source));
             }
