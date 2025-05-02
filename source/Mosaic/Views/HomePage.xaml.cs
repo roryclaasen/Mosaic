2b78574a36f10da91a8bcf6e17568c77834f13c3
Rory Claasen <rory@claasen.biz>
2025-05-02 22:24:19 +0100

    Add VideoMosaic and VideoPlayerTile components
    Created VideoMosaic and VideoPlayerTile user controls to manage video playback in a grid layout. Updated project files to include necessary dependencies and modified HomePage to integrate the new components. Enhanced functionality for video management, including play, pause, mute, and resizing features.

--- source/Mosaic/Views/HomePage.xaml.cs
+++ source/Mosaic/Views/HomePage.xaml.cs
@@ -4,10 +4,18 @@
 namespace Mosaic.Views;
 
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Mosaic.Controls;
+using Mosaic.Infrastructure;
+using Mosaic.Infrastructure.Config;
 using Mosaic.Util;
 using Windows.Foundation.Metadata;
 using Windows.Storage.Pickers;
@@ -15,8 +23,18 @@ using WinRT.Interop;
 
 public sealed partial class HomePage : Page
 {
+    private readonly MosaicManager mosaicManager;
+    private readonly CsvConfiguration csvConfiguration;
+
     public HomePage()
     {
+        this.mosaicManager = new MosaicManager();
+        this.csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = false,
+            IgnoreBlankLines = true
+        };
+
         this.InitializeComponent();
     }
 
@@ -49,50 +67,42 @@ public sealed partial class HomePage : Page
             InitializeWithWindow.Initialize(filePicker, WindowHelper.GetWindowHandleForCurrentWindow(startupWindow));
         }
 
-        // var file = await filePicker.PickSingleFileAsync();
-        // if (file is not null)
-        // {
-        //     using var steamReader = new StreamReader(await file.OpenStreamForReadAsync());
-        //     using var csvReader = new CsvReader(steamReader, this.csvConfiguration);
+        var file = await filePicker.PickSingleFileAsync();
+        if (file is not null)
+        {
+            using var steamReader = new StreamReader(await file.OpenStreamForReadAsync());
+            using var csvReader = new CsvReader(steamReader, this.csvConfiguration);
 
-        // this.SetVideoSources(csvReader.GetRecords<MediaEntry>());
-        // }
+            this.SetVideoSources(csvReader.GetRecords<MediaEntry>());
+        }
     }
 
-    private void CommandBar_LoadExampleVideos(object sender, RoutedEventArgs e)
-    {
-        string[] exampleVideos =
-        [
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"
-        ];
-
-        // this.SetVideoSources(exampleVideos.Select(mrl => new MediaEntry(new Uri(mrl))));
-    }
+    private void CommandBar_LoadExampleVideos(object sender, RoutedEventArgs e) => this.SetVideoSources([
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"
+    ]);
 
     private void CommandBar_Play(object sender, RoutedEventArgs e)
     {
-        // if (this.mosaicManager.SourceCount > 0)
-        // {
-        //     this.MosaicGrid.Play();
-        // }
+        if (this.mosaicManager.SourceCount > 0)
+        {
+            this.MosaicGrid.Play();
+        }
     }
 
     private void CommandBar_Stop(object sender, RoutedEventArgs e)
-    {
-        // this.MosaicGrid.Stop();
-    }
+        => this.MosaicGrid.Stop();
 
     private void CommandBar_ChangeTheme(object sender, RoutedEventArgs e)
     {
@@ -116,29 +126,29 @@ public sealed partial class HomePage : Page
     {
         var oldValue = (int)e.OldValue;
         var newValue = (int)e.NewValue;
-        // if (oldValue == newValue || this.MosaicGrid is null || sender is not Slider slider)
-        // {
-        //    return;
-        // }
+        if (oldValue == newValue || this.MosaicGrid is null || sender is not Slider slider)
+        {
+            return;
+        }
 
-        // switch (slider.Tag)
-        // {
-        //    case "m_width":
-        //        this.MosaicGrid.MosaicWidth = newValue;
-        //        break;
-        //    case "m_height":
-        //        this.MosaicGrid.MosaicHeight = newValue;
-        //        break;
-        //    default:
-        //        throw new Exception("Unknown slider tag");
-        // }
+        switch (slider.Tag)
+        {
+            case "m_width":
+                this.MosaicGrid.MosaicWidth = newValue;
+                break;
+            case "m_height":
+                this.MosaicGrid.MosaicHeight = newValue;
+                break;
+            default:
+                throw new Exception("Unknown slider tag");
+        }
     }
 
     private void CommandBar_ToggleLabels(object sender, RoutedEventArgs e)
     {
         if (sender is ToggleSwitch toggleSwitch)
         {
-            // this.MosaicGrid.SetShowLabels(toggleSwitch.IsOn);
+            this.MosaicGrid?.SetShowLabels(toggleSwitch.IsOn);
         }
     }
 
@@ -146,19 +156,22 @@ public sealed partial class HomePage : Page
     {
         if (sender is ToggleSwitch toggleSwitch)
         {
-            // this.MosaicGrid.SetMute(toggleSwitch.IsOn);
+            this.MosaicGrid?.SetMute(toggleSwitch.IsOn);
         }
     }
 
-    // private void SetVideoSources(IEnumerable<MediaEntry> entries)
-    // {
-    //    var wasPlaying = this.MosaicGrid.IsPlaying;
-    //    if (wasPlaying)
-    //    {
-    //        this.MosaicGrid.Stop();
-    //    }
+    private void SetVideoSources(IEnumerable<string> entries)
+        => this.SetVideoSources(entries.Select(x => new MediaEntry(new Uri(x))));
 
-    // this.mosaicManager.SetConfig(entries);
-    //    this.MosaicGrid.Play();
-    // }
+    private void SetVideoSources(IEnumerable<MediaEntry> entries)
+    {
+        var wasPlaying = this.MosaicGrid.IsPlaying;
+        if (wasPlaying)
+        {
+            this.MosaicGrid.Stop();
+        }
+
+        this.mosaicManager.SetConfig(entries);
+        this.MosaicGrid.Play();
+    }
 }
